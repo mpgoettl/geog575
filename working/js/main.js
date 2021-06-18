f1afd3166f33f0eec50db18e142bcf67314305eb
mpgoettl <78650176+mpgoettl@users.noreply.github.com>
2021-06-18 17:01:19 -0500

    update

--- working/js/main.js
+++ working/js/main.js
@@ -1,38 +1,38 @@
 /* Stylesheet by Martin P. Goettl, 2021 */
 /*console.log("hello world!");*/
 
-	$(document).ready(function() {
+$(document).ready(function() {
 
-		var cities;	
-		var map = L.map("map", { 
-			defaultExtentControl: true,
-			center: [39.34, -99.85], 
-			zoom: 4,	
-			minZoom: 4,
-			scrollWheelZoom: true,
-			tap: false
-			
-		});				
-					// display Stamen_TonerLite tiles with light features and labels
-		L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
-			attribution: 'Map tiles by &copy <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'})
-  
+	var cities;	
+	var map = L.map("map", { 
+		defaultExtentControl: true,
+		center: [39.34, -99.85], 
+		zoom: 4,	
+		minZoom: 4,
+		scrollWheelZoom: true,
+		tap: false
 		
-		.addTo(map);
-		
-	});
+	});				
+				// display Stamen_TonerLite tiles with light features and labels
+	L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+		attribution: 'Map tiles by &copy <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'})
+
 	
-	$.getJSON("data/cityData.json")
-		.done(function(data) {
-			var info = processData(data);
-			createPropSymbols(info.timestamps, data);
-		 })
-	.fail(function() { alert("There has been a problem loading the data.")});
+	.addTo(map);
+	
+});
+
+$.getJSON("data/cityData.json")
+	.done(function(data) {
+		var info = processData(data);
+		createPropSymbols(info.timestamps, data);
+	 })
+.fail(function() { alert("There has been a problem loading the data.")});
 	
 	
 	
 	
-(function processData(data) {
+function processData(data) {
 	var timestamps = [];
 	var min = Infinity;
 	var max = -Infinity;
@@ -68,10 +68,10 @@
 		min : min,
 		max : max
 	}
-})
+}
 
-(function createPropSymbols(timestamps, data) {
+function createPropSymbols(timestamps, data) {
 		
 	cities = L.geoJson(data).addTo(map); 
 	
-})
+}
